595efb24d68fe291fd19442a9e935751a77aebbc
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2009-11-08 17:31:03 +0000

    Using non-modified versions of commons-math and JAS

---
+++ trunk/jas/src/main/java/edu/jas/root/Interval.java
@@ -0,0 +1,166 @@
+/*
+ * $Id: Interval.java 2756 2009-07-18 11:13:30Z kredel $
+ */
+
+package edu.jas.root;
+
+
+import edu.jas.arith.BigDecimal;
+import edu.jas.arith.BigRational;
+import edu.jas.arith.ToRational;
+import edu.jas.structure.RingElem;
+
+
+/**
+ * Interval. For example isolating interval for real roots.
+ * @param <C> coefficient type.
+ * @author Heinz Kredel
+ */
+public class Interval<C extends RingElem<C> & ToRational > {
+
+
+    /**
+     * left interval border.
+     */
+    public final C left;
+
+
+    /**
+     * right interval border.
+     */
+    public final C right;
+
+
+    /**
+     * Constructor.
+     * @param left interval border.
+     * @param right interval border.
+     */
+    public Interval(C left, C right) {
+        this.left = left;
+        this.right = right;
+    }
+
+
+    /**
+     * Constructor.
+     * @param mid left and right interval border.
+     */
+    public Interval(C mid) {
+        this(mid, mid);
+    }
+
+
+    /**
+     * String representation of Interval.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "[" + left + ", " + right + "]";
+    }
+
+
+    /**
+     * Get a scripting compatible string representation.
+     * @return script compatible representation for this Interval.
+     */
+    public String toScript() {
+        // Python case
+        return "( " + left + ", " + right + " ) ";
+    }
+
+
+    /**
+     * Clone this.
+     * @see java.lang.Object#clone()
+     */
+    @Override
+    public Interval<C> clone() {
+        return new Interval<C>(left, right);
+    }
+
+
+    /**
+     * Comparison with any other object.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object b) {
+        if (!(b instanceof Interval)) {
+            return false;
+        }
+        Interval<C> a = null;
+        try {
+            a = (Interval<C>) b;
+        } catch (ClassCastException e) {
+        }
+        return left.equals(a.left) && right.equals(a.right);
+    }
+
+
+    /**
+     * Hash code for this Interval.
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return 37 * left.hashCode() + right.hashCode();
+    }
+
+
+    /**
+     * Length.
+     * @return |left-right|;
+     */
+    public C length() {
+        C m = right.subtract(left);
+        return m.abs();
+    }
+
+
+    /**
+     * BigDecimal representation of Interval.
+     */
+    public BigDecimal toDecimal() {
+        BigDecimal l = new BigDecimal(left.toRational());
+        BigDecimal r = new BigDecimal(right.toRational());
+        BigDecimal two = new BigDecimal(2);
+        BigDecimal v = l.sum(r).divide(two);
+        return v;
+    }
+//         if ((Object) left instanceof BigRational) {
+//             BigDecimal l = new BigDecimal(left.toRational());
+//             BigDecimal r = new BigDecimal(right.toRational());
+//             //BigDecimal l = new BigDecimal((BigRational)(Object)left);
+//             //BigDecimal r = new BigDecimal((BigRational)(Object)right);
+//             BigDecimal two = new BigDecimal(2);
+//             BigDecimal v = l.sum(r).divide(two);
+//             return v;
+//         } else if ((Object) left instanceof RealAlgebraicNumber) {
+//             //RealAlgebraicNumber x = (RealAlgebraicNumber) left;
+//             //RealAlgebraicNumber y = (RealAlgebraicNumber) right;
+//             //BigDecimal l = new BigDecimal(x.magnitude());
+//             //BigDecimal r = new BigDecimal(y.magnitude());
+//             BigDecimal l = new BigDecimal(left.toRational());
+//             BigDecimal r = new BigDecimal(right.toRational());
+//             BigDecimal two = new BigDecimal(2);
+//             BigDecimal v = l.sum(r).divide(two);
+//             return v;
+//         } else {
+//             throw new RuntimeException("toDecimal of interval types not implemented");
+//         }
+
+
+    /**
+     * Rational middle point.
+     * @return (left-right)/2;
+     */
+    public BigRational rationalMiddle() {
+        BigRational m = left.toRational().sum(right.toRational());
+        BigRational t = new BigRational(1L,2L);
+        m = m.multiply(t);
+        return m;
+    }
+}
---
+++ trunk/jas/src/main/java/edu/jas/ufd/FactorModular.java
@@ -0,0 +1,213 @@
+/*
+ * $Id: FactorModular.java 2621 2009-05-03 17:17:03Z kredel $
+ */
+
+package edu.jas.ufd;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import org.apache.log4j.Logger;
+
+import edu.jas.arith.BigInteger;
+import edu.jas.arith.ModInteger;
+import edu.jas.arith.ModIntegerRing;
+import edu.jas.poly.GenPolynomial;
+import edu.jas.poly.GenPolynomialRing;
+import edu.jas.structure.Power;
+import edu.jas.structure.RingFactory;
+
+
+/**
+ * Modular coefficients factorization algorithms.
+ * This class implements factorization methods for polynomials over (prime) modular integers.
+ * @author Heinz Kredel
+ */
+
+public class FactorModular extends FactorAbsolute<ModInteger> {
+
+
+    private static final Logger logger = Logger.getLogger(FactorModular.class);
+
+
+    private final boolean debug = true || logger.isDebugEnabled();
+
+
+    /**
+     * No argument constructor.
+     */
+    public FactorModular() {
+        this( new ModIntegerRing(13,true) ); // hack, 13 unimportant
+    }
+
+
+    /**
+     * Constructor. 
+     * @param cfac coefficient ring factory.
+     */
+    public FactorModular(RingFactory<ModInteger> cfac) {
+        super(cfac);
+    }
+
+
+    /**
+     * GenPolynomial base distinct degree factorization.
+     * @param P squarefree and monic GenPolynomial.
+     * @return [e_1 -&gt; p_1, ..., e_k -&gt; p_k] with P = prod_{i=1,...,k} p_i and
+     *         p_i has only irreducible factors of degree e_i.
+     */
+    public SortedMap<Long, GenPolynomial<ModInteger>> baseDistinctDegreeFactors(GenPolynomial<ModInteger> P) {
+        if (P == null) {
+            throw new RuntimeException(this.getClass().getName() + " P != null");
+        }
+        SortedMap<Long, GenPolynomial<ModInteger>> facs = new TreeMap<Long, GenPolynomial<ModInteger>>();
+        if (P.isZERO()) {
+            return facs;
+        }
+        GenPolynomialRing<ModInteger> pfac = P.ring;
+        if (pfac.nvar > 1) {
+            throw new RuntimeException(this.getClass().getName() + " only for univariate polynomials");
+        }
+        ModIntegerRing mr = (ModIntegerRing) pfac.coFac;
+        java.math.BigInteger m = mr.modul;
+        if (m.longValue() == 2L) {
+            throw new RuntimeException(this.getClass().getName() + " case p = 2 not implemented");
+        }
+        GenPolynomial<ModInteger> x = pfac.univariate(0);
+        GenPolynomial<ModInteger> h = x;
+        GenPolynomial<ModInteger> f = P;
+        GenPolynomial<ModInteger> g;
+        GreatestCommonDivisor<ModInteger> engine = GCDFactory.<ModInteger> getImplementation(pfac.coFac);
+        Power<GenPolynomial<ModInteger>> pow = new Power<GenPolynomial<ModInteger>>(pfac);
+        long d = 0;
+        while (d + 1 <= f.degree(0) / 2) {
+            d++;
+            h = pow.modPower(h, m, f);
+            g = engine.gcd(h.subtract(x), f);
+            if (!g.isONE()) {
+                facs.put(d, g);
+                f = f.divide(g);
+            }
+        }
+        if (!f.isONE()) {
+            d = f.degree(0);
+            facs.put(d, f);
+        }
+        return facs;
+    }
+
+
+    /**
+     * GenPolynomial base equal degree factorization.
+     * @param P squarefree and monic GenPolynomial.
+     * @param deg such that P has only irreducible factors of degree deg.
+     * @return [p_1,...,p_k] with P = prod_{i=1,...,r} p_i.
+     */
+    public List<GenPolynomial<ModInteger>> baseEqualDegreeFactors(GenPolynomial<ModInteger> P, long deg) {
+        if (P == null) {
+            throw new RuntimeException(this.getClass().getName() + " P != null");
+        }
+        List<GenPolynomial<ModInteger>> facs = new ArrayList<GenPolynomial<ModInteger>>();
+        if (P.isZERO()) {
+            return facs;
+        }
+        GenPolynomialRing<ModInteger> pfac = P.ring;
+        if (pfac.nvar > 1) {
+            throw new RuntimeException(this.getClass().getName() + " only for univariate polynomials");
+        }
+        if (P.degree(0) == deg) {
+            facs.add(P);
+            return facs;
+        }
+        ModIntegerRing mr = (ModIntegerRing) pfac.coFac;
+        java.math.BigInteger m = mr.modul;
+        //System.out.println("m = " + m);
+        if (m.equals(java.math.BigInteger.valueOf(2L))) {
+            throw new RuntimeException(this.getClass().getName() + " case p = 2 not implemented");
+        }
+        GenPolynomial<ModInteger> one = pfac.getONE();
+        GenPolynomial<ModInteger> r;
+        GenPolynomial<ModInteger> h;
+        GenPolynomial<ModInteger> f = P;
+        GreatestCommonDivisor<ModInteger> engine = GCDFactory.<ModInteger> getImplementation(pfac.coFac);
+        Power<GenPolynomial<ModInteger>> pow = new Power<GenPolynomial<ModInteger>>(pfac);
+        GenPolynomial<ModInteger> g = null;
+        int degi = (int) deg; //f.degree(0);
+        //System.out.println("deg = " + deg);
+        BigInteger di = Power.<BigInteger> positivePower(new BigInteger(m), deg);
+        //System.out.println("di = " + di);
+        java.math.BigInteger d = di.getVal(); //.longValue()-1;
+        //System.out.println("d = " + d);
+        d = d.shiftRight(1); // divide by 2
+        do {
+            r = pfac.random(17, degi, 2 * degi, 1.0f);
+            if (r.degree(0) >= f.degree(0)) {
+                r = r.remainder(f);
+            }
+            r = r.monic();
+            //System.out.println("r = " + r);
+            h = pow.modPower(r, d, f);
+            g = engine.gcd(h.subtract(one), f);
+            //System.out.println("g = " + g);
+            degi++;
+        } while (g.degree(0) == 0 || g.degree(0) == f.degree(0));
+        f = f.divide(g);
+        facs.addAll(baseEqualDegreeFactors(f, deg));
+        facs.addAll(baseEqualDegreeFactors(g, deg));
+        return facs;
+    }
+
+
+    /**
+     * GenPolynomial base factorization of a squarefree polynomial.
+     * @param P squarefree and monic! GenPolynomial.
+     * @return [p_1,...,p_k] with P = prod_{i=1,...,r} p_i.
+     */
+    @Override
+    public List<GenPolynomial<ModInteger>> baseFactorsSquarefree(GenPolynomial<ModInteger> P) {
+        if (P == null) {
+            throw new RuntimeException(this.getClass().getName() + " P == null");
+        }
+        List<GenPolynomial<ModInteger>> factors = new ArrayList<GenPolynomial<ModInteger>>();
+        if (P.isZERO()) {
+            return factors;
+        }
+        if (P.isONE()) {
+            factors.add(P);
+            return factors;
+        }
+        GenPolynomialRing<ModInteger> pfac = P.ring;
+        if (pfac.nvar > 1) {
+            throw new RuntimeException(this.getClass().getName() + " only for univariate polynomials");
+        }
+        if (!P.leadingBaseCoefficient().isONE()) {
+            throw new RuntimeException("ldcf(P) != 1: " + P);
+        }
+        SortedMap<Long, GenPolynomial<ModInteger>> dfacs = baseDistinctDegreeFactors(P);
+        if ( debug ) {
+            logger.info("dfacs    = " + dfacs);
+            //System.out.println("dfacs    = " + dfacs);
+        }
+        for (Long e : dfacs.keySet()) {
+            GenPolynomial<ModInteger> f = dfacs.get(e);
+            List<GenPolynomial<ModInteger>> efacs = baseEqualDegreeFactors(f, e);
+            if ( debug ) {
+               logger.info("efacs " + e + "   = " + efacs);
+               //System.out.println("efacs " + e + "   = " + efacs);
+            }
+            factors.addAll(efacs);
+        }
+        //System.out.println("factors  = " + factors);
+        SortedSet<GenPolynomial<ModInteger>> ss = new TreeSet<GenPolynomial<ModInteger>>(factors);
+        //System.out.println("sorted   = " + ss);
+        factors.clear();
+        factors.addAll(ss);
+        return factors;
+    }
+
+}
