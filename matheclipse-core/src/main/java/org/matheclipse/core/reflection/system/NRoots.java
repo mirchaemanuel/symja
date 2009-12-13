86a47accdfc807e3431f781bb4f1e58c751154e1
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2009-12-13 16:34:04 +0000

    Added NRoots[] function

---
+++ matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/NRoots.java
@@ -0,0 +1,39 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+
+/**
+ * Determine the numerical roots of a univariate polynomial
+ * 
+ * See Wikipedia entries for: <a
+ * href="http://en.wikipedia.org/wiki/Quadratic_equation">Quadratic equation
+ * </a>, <a href="http://en.wikipedia.org/wiki/Cubic_function">Cubic
+ * function</a> and <a
+ * href="http://en.wikipedia.org/wiki/Quartic_function">Quartic function</a>
+ * 
+ * @see Roots
+ */
+public class NRoots extends Roots {
+
+  public NRoots() {
+  }
+
+  @Override
+  public IExpr evaluate(final IAST lst) {
+    if (lst.size() != 2) {
+      return null;
+    }
+    IAST list = roots(lst);
+    if (list == null) {
+      return null;
+    }
+    IAST result = F.List();
+    for (int i = 1; i < list.size(); i++) {
+      result.add(F.evaln(list.get(i)));
+    }
+    return result;
+  }
+
+}
