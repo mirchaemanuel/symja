024e3bd53e68e6c6d7ef981d036f3b146c76a3fc
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2010-04-14 21:33:13 +0000

    Added Testcase for EvalEngine#evalStepByStep() method to show the steps of evaluating an expression

---
+++ trunk/matheclipse-core/src/test/java/org/matheclipse/core/system/EvalEngineTestCase.java
@@ -0,0 +1,118 @@
+package org.matheclipse.core.system;
+
+import org.matheclipse.core.convert.AST2Expr;
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.form.output.OutputFormFactory;
+import org.matheclipse.core.form.output.StringBufferWriter;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.Parser;
+import org.matheclipse.parser.client.ast.ASTNode;
+
+public class EvalEngineTestCase extends SpecialTestCase {
+	
+	public EvalEngineTestCase(String name) {
+		super(name);
+	}
+
+	public void testStepByStep() {
+		try {
+
+			EvalEngine engine = EvalEngine.get();
+			Parser parser = new Parser();
+			ASTNode node = parser.parse("(3*x)^2 + (5*x)^2");
+			// convert ASTNode to an IExpr node
+			IExpr expr = AST2Expr.CONST.convert(node);
+			IExpr result = null;
+
+			StringBufferWriter buf;
+			if (expr != null) {
+				// Step 1
+				result = engine.evalStepByStep(expr);
+				if (result != null) {
+					buf = new StringBufferWriter();
+					buf.setIgnoreNewLine(true);
+					// convert the result back to output form
+					OutputFormFactory.get().convert(buf, result);
+					assertEquals(buf.toString(), "5^2*x^2+3^2*x^2");
+				}
+				expr = result;
+			}
+			if (expr != null) {
+				// Step 2
+				result = engine.evalStepByStep(expr);
+				if (result != null) {
+					buf = new StringBufferWriter();
+					buf.setIgnoreNewLine(true);
+					OutputFormFactory.get().convert(buf, result);
+					assertEquals(buf.toString(), "25*x^2+9*x^2");
+				}
+				expr = result;
+			}
+			if (expr != null) {
+				// Step 3
+				result = engine.evalStepByStep(expr);
+				if (result != null) {
+					buf = new StringBufferWriter();
+					buf.setIgnoreNewLine(true);
+					OutputFormFactory.get().convert(buf, result);
+					// the Times[] expression occurs here, because the OneIdentity
+					// attribute for Times is evaluated after this step
+					assertEquals(buf.toString(), "(25+9)*Times[x^2]");
+				}
+				expr = result;
+			}
+			if (expr != null) {
+				// Step 3
+				result = engine.evalStepByStep(expr);
+				if (result != null) {
+					buf = new StringBufferWriter();
+					buf.setIgnoreNewLine(true);
+					OutputFormFactory.get().convert(buf, result);
+					assertEquals(buf.toString(), "(25+9)*x^2");
+				}
+				expr = result;
+			}
+
+			if (expr != null) {
+				// Step 4
+				result = engine.evalStepByStep(expr);
+				if (result != null) {
+					buf = new StringBufferWriter();
+					buf.setIgnoreNewLine(true);
+					OutputFormFactory.get().convert(buf, result);
+					assertEquals(buf.toString(), "34*x^2");
+				}
+				expr = result;
+			}
+
+			if (expr != null) {
+				// Step 5 
+				result = engine.evalStepByStep(expr);
+				// no further 
+				assertEquals(result, null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test extension with Package[] function
+	 */
+	public void testPackage001() {
+		check("Package[ \"PackageName\", {Test}, {\n" + "   Test[x_,y_]:=TestIntern[x,y],\n"
+				+ "   TestIntern[x_,y_] := {x,y,mySymbol}, \n" + "   mySymbol = 4711 \n" + " } ]", "");
+		check("Test[a,b]", "{a,b,4711}");
+		// the TestIntern rule should not be visible outside the package:
+		check("TestIntern[a,b]", "TestIntern[a,b]");
+		// the mySymbol constant should not be visible outside the package:
+		check("mySymbol", "mySymbol");
+		check("Test[a,b]", "{a,b,4711}");
+		// a new EvalEngine sees the new defined rules:
+		EvalEngine engine = new EvalEngine();
+		check(engine, false, "Test[a,b]", "{a,b,4711}", false);
+		// this should give an syntax error:
+		// check("@1TestIntern[a,b]","");
+	}
+
+}
