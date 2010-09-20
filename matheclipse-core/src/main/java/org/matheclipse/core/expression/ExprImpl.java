b7f85380e8f52eba69b636a16f2d92b47357ab26
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2010-09-20 06:08:11 +0000

    Issue #17: Fixed ClassCastException http://code.google.com/p/symja/issues/detail?id=17

--- matheclipse-core/src/main/java/org/matheclipse/core/expression/ExprImpl.java
+++ matheclipse-core/src/main/java/org/matheclipse/core/expression/ExprImpl.java
@@ -14,233 +14,243 @@ import org.matheclipse.core.patternmatching.PatternMatcher;
 import apache.harmony.math.BigInteger;
 
 public abstract class ExprImpl implements IExpr {
-  // protected int hash;
+	// protected int hash;
 
-  // @Override
-  // public Field<IExpr> getField() {
-  // return ExprFieldOld.CONST;
-  // }
+	// @Override
+	// public Field<IExpr> getField() {
+	// return ExprFieldOld.CONST;
+	// }
 
-  public IExpr opposite() {
-    return F.function(F.Times, F.CN1, this);
-  }
+	public IExpr opposite() {
+		return F.function(F.Times, F.CN1, this);
+	}
 
-  /**
-   * Additional negative method, which works like opposite to fulfill groovy's
-   * method signature
-   * 
-   * @return
-   */
-  public final IExpr negative() {
-    return opposite();
-  }
+	/**
+	 * Additional negative method, which works like opposite to fulfill groovy's
+	 * method signature
+	 * 
+	 * @return
+	 */
+	public final IExpr negative() {
+		return opposite();
+	}
 
-  public IExpr minus(final IExpr that) {
-    return F.function(F.Plus, this, F.function(F.Times, F.CN1, that));
-  }
+	public IExpr minus(final IExpr that) {
+		return F.function(F.Plus, this, F.function(F.Times, F.CN1, that));
+	}
 
-  public IExpr plus(final IExpr that) {
-    return F.function(F.Plus, this, that);
-  }
+	public IExpr plus(final IExpr that) {
+		return F.function(F.Plus, this, that);
+	}
 
-  public IExpr inverse() {
-    return F.function(F.Power, this, F.CN1);
-  }
+	public IExpr inverse() {
+		return F.function(F.Power, this, F.CN1);
+	}
 
-  public IExpr times(final IExpr that) {
-    return F.function(F.Times, this, that);
-  }
+	public IExpr times(final IExpr that) {
+		return F.function(F.Times, this, that);
+	}
 
-  /**
-   * Additional multiply method which works like times to fulfill groovy's
-   * method signature
-   * 
-   * @param that
-   * @return
-   */
-  public final IExpr multiply(final IExpr that) {
-    return times(that);
-  }
+	/**
+	 * Additional multiply method which works like times to fulfill groovy's
+	 * method signature
+	 * 
+	 * @param that
+	 * @return
+	 */
+	public final IExpr multiply(final IExpr that) {
+		return times(that);
+	}
 
-  public final IExpr power(final Integer n) {
-    return F.function(F.Power, this, F.integer(n));
-  }
+	public final IExpr power(final Integer n) {
+		return F.function(F.Power, this, F.integer(n));
+	}
 
-  public final IExpr power(final IExpr that) {
-    return F.function(F.Power, this, that);
-  }
+	public final IExpr power(final IExpr that) {
+		return F.function(F.Power, this, that);
+	}
 
-  public IExpr div(final IExpr that) {
-    return F.eval(F.function(F.Times, this, F.function(F.Power, that, F.CN1)));
-  }
+	public IExpr div(final IExpr that) {
+		return F.eval(F.function(F.Times, this, F.function(F.Power, that, F.CN1)));
+	}
 
-  public IExpr mod(final IExpr that) {
-    return F.function(F.Mod, this, that);
-  }
+	public IExpr mod(final IExpr that) {
+		return F.function(F.Mod, this, that);
+	}
 
-  public IExpr and(final IExpr that) {
-    return F.function(F.And, this, that);
-  }
+	public IExpr and(final IExpr that) {
+		return F.function(F.And, this, that);
+	}
 
-  public IExpr or(final IExpr that) {
-    return F.function(F.Or, this, that);
-  }
+	public IExpr or(final IExpr that) {
+		return F.function(F.Or, this, that);
+	}
 
-  public IExpr getAt(final int index) {
-    return F.function(F.Part, this, F.integer(index));
-  }
+	public IExpr getAt(final int index) {
+		return F.function(F.Part, this, F.integer(index));
+	}
 
-  public Object asType(Class clazz) {
-    if (clazz.equals(Boolean.class)) {
-      if (this.equals(F.True)) {
-        return Boolean.TRUE;
-      }
-      if (this.equals(F.False)) {
-        return Boolean.FALSE;
-      }
-    } else if (clazz.equals(Integer.class)) {
-      if (this instanceof IInteger) {
-        return Integer.valueOf(((IInteger) this).toInt());
-      }
-    } else if (clazz.equals(java.math.BigInteger.class)) {
-      if (this instanceof IntegerSym) {
-        return new BigInteger(((IntegerSym) this).toByteArray());
-      }
-    } else if (clazz.equals(String.class)) {
-      return toString();
-    }
-    throw new UnsupportedOperationException(
-        "ExprImpl.asType() - cast not supported.");
-  }
+	public Object asType(Class clazz) {
+		if (clazz.equals(Boolean.class)) {
+			if (this.equals(F.True)) {
+				return Boolean.TRUE;
+			}
+			if (this.equals(F.False)) {
+				return Boolean.FALSE;
+			}
+		} else if (clazz.equals(Integer.class)) {
+			if (this instanceof IInteger) {
+				return Integer.valueOf(((IInteger) this).toInt());
+			}
+		} else if (clazz.equals(java.math.BigInteger.class)) {
+			if (this instanceof IntegerSym) {
+				return new BigInteger(((IntegerSym) this).toByteArray());
+			}
+		} else if (clazz.equals(String.class)) {
+			return toString();
+		}
+		throw new UnsupportedOperationException("ExprImpl.asType() - cast not supported.");
+	}
 
-  public abstract ISymbol head();
+	public abstract ISymbol head();
 
-  public ISymbol topHead() {
-    return head();
-  }
+	public ISymbol topHead() {
+		return head();
+	}
 
-  public boolean isList() {
-    return false;
-  }
+	public boolean isList() {
+		return false;
+	}
 
-  public boolean isTrue() {
-    return false;
-  }
+	public boolean isTrue() {
+		return false;
+	}
 
-  public boolean isFalse() {
-    return false;
-  }
+	public boolean isFalse() {
+		return false;
+	}
 
-  public boolean isSame(IExpr expression) {
-    return isSame(expression, Config.DOUBLE_EPSILON);
-  }
+	public boolean isSame(IExpr expression) {
+		return isSame(expression, Config.DOUBLE_EPSILON);
+	}
 
-  public boolean isSame(IExpr expression, double epsilon) {
-    return equals(expression);
-  }
+	public boolean isSame(IExpr expression, double epsilon) {
+		return equals(expression);
+	}
 
-  public int[] isMatrix() {
-    // default: no matrix
-    return null;
-  }
+	public int[] isMatrix() {
+		// default: no matrix
+		return null;
+	}
 
-  public int isVector() {
-    // default: no vector
-    return -1;
-  }
+	public int isVector() {
+		// default: no vector
+		return -1;
+	}
 
-  public boolean isAST(final IExpr header) {
-    return false;
-  }
+	public boolean isAST(final IExpr header) {
+		return false;
+	}
 
-  public boolean isAST(final IExpr header, final int sz) {
-    return false;
-  }
+	public boolean isAST(final IExpr header, final int sz) {
+		return false;
+	}
 
-  public boolean isASTSizeGE(final IExpr header, final int size) {
-    return false;
-  }
+	public boolean isASTSizeGE(final IExpr header, final int size) {
+		return false;
+	}
 
-  public boolean isAST(final String symbol) {
-    return false;
-  }
+	public boolean isAST(final String symbol) {
+		return false;
+	}
 
-  public boolean isAST(final String symbol, final int size) {
-    return false;
-  }
+	public boolean isAST(final String symbol, final int size) {
+		return false;
+	}
 
-  public boolean isFree(final IExpr pattern) {
-    final PatternMatcher matcher = new PatternMatcher(pattern);
-    return !AST.COPY.some(this, matcher, 1);
-  }
+	public boolean isPlus() {
+		return false;
+	}
 
-  public boolean isNumber() {
-    return this instanceof INumber;
-  }
+	public boolean isPower() {
+		return false;
+	}
 
-  public boolean isLTOrdered(final IExpr obj) {
-    return compareTo(obj) < 0;
-  }
+	public boolean isTimes() {
+		return false;
+	}
 
-  public boolean isLEOrdered(final IExpr obj) {
-    return compareTo(obj) <= 0;
-  }
+	public boolean isFree(final IExpr pattern) {
+		final PatternMatcher matcher = new PatternMatcher(pattern);
+		return !AST.COPY.some(this, matcher, 1);
+	}
 
-  public boolean isGTOrdered(final IExpr obj) {
-    return compareTo(obj) > 0;
-  }
+	public boolean isNumber() {
+		return this instanceof INumber;
+	}
 
-  public boolean isGEOrdered(final IExpr obj) {
-    return compareTo(obj) >= 0;
-  }
+	public boolean isLTOrdered(final IExpr obj) {
+		return compareTo(obj) < 0;
+	}
 
-  public boolean isAtom() {
-    return true;
-  }
+	public boolean isLEOrdered(final IExpr obj) {
+		return compareTo(obj) <= 0;
+	}
 
-  // public INestedList castTo() {
-  // return null;
-  // }
+	public boolean isGTOrdered(final IExpr obj) {
+		return compareTo(obj) > 0;
+	}
 
-  public IExpr variables2Slots(final Map<IExpr, IExpr> map,
-      final List<IExpr> variableList) {
-    return this;
-  }
+	public boolean isGEOrdered(final IExpr obj) {
+		return compareTo(obj) >= 0;
+	}
 
-  // public IExpr save() {
-  // return (IExpr) super.export();
-  // }
-  //
-  // public IExpr saveHeap() {
-  // return (IExpr) super.moveHeap();
-  // }
+	public boolean isAtom() {
+		return true;
+	}
 
-  public String fullFormString() {
-    return toString();
-  }
+	// public INestedList castTo() {
+	// return null;
+	// }
 
-  public String internalFormString() {
-    return toString();
-  }
-  
-  public List<IExpr> leaves() {
-    return null;
-  }
+	public IExpr variables2Slots(final Map<IExpr, IExpr> map, final List<IExpr> variableList) {
+		return this;
+	}
 
-  public IExpr apply(List<? extends IExpr> leaves) {
-    final IAST ast = F.ast(head());
-    for (int i = 0; i < leaves.size(); i++) {
-      ast.add(leaves.get(i));
-    }
-    return ast;
-  }
+	// public IExpr save() {
+	// return (IExpr) super.export();
+	// }
+	//
+	// public IExpr saveHeap() {
+	// return (IExpr) super.moveHeap();
+	// }
 
-  public IExpr apply(IExpr... leaves) {
-    final IAST ast = F.ast(head());
-    for (int i = 0; i < leaves.length; i++) {
-      ast.add(leaves[i]);
-    }
-    return ast;
-  }
+	public String fullFormString() {
+		return toString();
+	}
+
+	public String internalFormString() {
+		return toString();
+	}
+
+	public List<IExpr> leaves() {
+		return null;
+	}
+
+	public IExpr apply(List<? extends IExpr> leaves) {
+		final IAST ast = F.ast(head());
+		for (int i = 0; i < leaves.size(); i++) {
+			ast.add(leaves.get(i));
+		}
+		return ast;
+	}
+
+	public IExpr apply(IExpr... leaves) {
+		final IAST ast = F.ast(head());
+		for (int i = 0; i < leaves.length; i++) {
+			ast.add(leaves[i]);
+		}
+		return ast;
+	}
 
 }
