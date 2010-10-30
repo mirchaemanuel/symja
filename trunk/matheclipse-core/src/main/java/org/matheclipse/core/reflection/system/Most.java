1824c1beafa407a086c98c242bcc57bf29993d1e
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2010-10-30 20:05:32 +0000

    Refactorings

--- trunk/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Most.java
+++ trunk/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Most.java
@@ -1,5 +1,6 @@
 package org.matheclipse.core.reflection.system;
 
+import org.matheclipse.core.eval.exception.Validate;
 import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
@@ -10,16 +11,14 @@ public class Most extends AbstractFunctionEvaluator {
 
 	public Most() {
 	}
-
+  
 	@Override
 	public IExpr evaluate(final IAST ast) {
-		if ((ast.size()!=2) || !(ast.get(1) instanceof IAST)) {
-			return null;
-		}
-		final IAST sublist = ((IAST)ast.get(1)).clone();
+		Validate.checkSize(ast, 2);
+		final IAST sublist = Validate.checkASTType(ast, 1).clone();
 
 		if (sublist.size() > 1) {
-			sublist.remove(sublist.size()-1);
+			sublist.remove(sublist.size() - 1);
 			return sublist;
 		}
 
--- trunk/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Rest.java
+++ trunk/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Rest.java
@@ -1,12 +1,13 @@
 package org.matheclipse.core.reflection.system;
 
+import org.matheclipse.core.eval.exception.Validate;
 import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 
 /**
- * Return the <i>rest</i> of a given list, i.e. a sublist with all elements
- * from list[[2]]...list[[n]]
+ * Return the <i>rest</i> of a given list, i.e. a sublist with all elements from
+ * list[[2]]...list[[n]]
  */
 public class Rest extends AbstractFunctionEvaluator {
 
@@ -14,11 +15,9 @@ public class Rest extends AbstractFunctionEvaluator {
 	}
 
 	@Override
-	public IExpr evaluate(final IAST functionList) {
-		if ((functionList.size()!=2) || !(functionList.get(1) instanceof IAST)) {
-			return null;
-		}
-		final IAST sublist = ((IAST)functionList.get(1)).clone();
+	public IExpr evaluate(final IAST ast) {
+		Validate.checkSize(ast, 2);
+		final IAST sublist = Validate.checkASTType(ast, 1).clone();
 
 		if (sublist.size() > 1) {
 			sublist.remove(1);
