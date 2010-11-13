6b413b184b9cc0afb2cbfd21e4b558e525289b48
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2010-11-13 13:49:07 +0000

    Check for iteration limits

--- trunk/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/While.java
+++ trunk/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/While.java
@@ -3,6 +3,8 @@ package org.matheclipse.core.reflection.system;
 import org.matheclipse.core.eval.EvalEngine;
 import org.matheclipse.core.eval.exception.BreakException;
 import org.matheclipse.core.eval.exception.ContinueException;
+import org.matheclipse.core.eval.exception.IterationLimitExceeded;
+import org.matheclipse.core.eval.exception.Validate;
 import org.matheclipse.core.eval.interfaces.IFunctionEvaluator;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.interfaces.IAST;
@@ -16,18 +18,26 @@ public class While implements IFunctionEvaluator {
 	}
 
 	public IExpr evaluate(final IAST ast) {
+		Validate.checkSize(ast, 3);
 		final EvalEngine engine = EvalEngine.get();
-		if (ast.size() == 3) {
-			while (engine.evaluate(ast.get(1)).equals(F.True)) {
-				try {
-					engine.evaluate(ast.get(2));
-				} catch (final BreakException e) {
-					return F.Null;
-				} catch (final ContinueException e) {
-					continue;
+
+		final int iterationLimit = engine.getIterationLimit();
+		int iterationCounter = 1;
+
+		while (engine.evaluate(ast.get(1)).equals(F.True)) {
+			try {
+				engine.evaluate(ast.get(2));
+
+				if (iterationLimit >= 0 && iterationLimit <= ++iterationCounter) {
+					IterationLimitExceeded.throwIt(iterationCounter, ast);
 				}
+			} catch (final BreakException e) {
+				return F.Null;
+			} catch (final ContinueException e) {
+				continue;
 			}
 		}
+
 		return F.Null;
 	}
 
