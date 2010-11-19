4274602602e5e7151f3fd3046bff5c4bac90b2ec
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2010-11-19 22:10:48 +0000

    Shrink org.matheclipse.core.eval.Console application with proGuard tool

--- trunk/matheclipse-core/src/main/java/org/matheclipse/core/expression/Pattern.java
+++ trunk/matheclipse-core/src/main/java/org/matheclipse/core/expression/Pattern.java
@@ -13,8 +13,6 @@ import org.matheclipse.core.visit.IVisitor;
 import org.matheclipse.core.visit.IVisitorBoolean;
 import org.matheclipse.core.visit.IVisitorInt;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import com.google.common.base.Predicate;
 
 /**
