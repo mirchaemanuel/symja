e54cbe5e6ac449ad00ef02f14ee59cdb0e9bc53f
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2011-07-23 14:29:15 +0000

    Use latest commons-math sources from SVN

--- matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/FindRoot.java
+++ matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/FindRoot.java
@@ -1,7 +1,6 @@
 package org.matheclipse.core.reflection.system;
 
 import org.apache.commons.math.ConvergenceException;
-import org.apache.commons.math.MathException;
 import org.apache.commons.math.analysis.DifferentiableUnivariateRealFunction;
 import org.apache.commons.math.analysis.UnivariateRealFunction;
 import org.apache.commons.math.analysis.solvers.BaseAbstractUnivariateRealSolver;
@@ -14,15 +13,19 @@ import org.apache.commons.math.analysis.solvers.PegasusSolver;
 import org.apache.commons.math.analysis.solvers.RegulaFalsiSolver;
 import org.apache.commons.math.analysis.solvers.RiddersSolver;
 import org.apache.commons.math.analysis.solvers.SecantSolver;
+import org.apache.commons.math.exception.MaxCountExceededException;
 import org.matheclipse.core.basic.Config;
 import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.exception.Validate;
 import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
+import org.matheclipse.core.eval.util.Options;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.expression.IConstantHeaders;
 import org.matheclipse.core.expression.Num;
 import org.matheclipse.core.generic.UnaryNumerical;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.IInteger;
 import org.matheclipse.core.interfaces.ISignedNumber;
 import org.matheclipse.core.interfaces.ISymbol;
 
@@ -42,37 +45,40 @@ public class FindRoot extends AbstractFunctionEvaluator implements IConstantHead
 
 	@Override
 	public IExpr evaluate(final IAST ast) {
-		if ((ast.size() == 3 || ast.size() == 4)) {
-			String method = "Newton";
-			if (ast.size() == 4 && ast.get(3) instanceof ISymbol) {
-				method = ast.get(3).toString();
+		Validate.checkRange(ast, 3);
+		
+		String method = "Newton";
+		int maxIterations = 100;
+		if (ast.size() >= 4) {
+			final Options options = new Options(ast.topHead(), ast, 3);
+			IExpr optionMaxIterations = options.getOption("MaxIterations");
+			if (optionMaxIterations != null && optionMaxIterations instanceof IInteger) {
+				maxIterations = ((IInteger) optionMaxIterations).toInt();
 			}
-			if ((ast.get(2).isList())) {
-				IAST list = (IAST) ast.get(2);
-				IExpr function = ast.get(1);
-				if (list.size() == 4 && list.get(1) instanceof ISymbol && list.get(2) instanceof ISignedNumber
-						&& list.get(3) instanceof ISignedNumber) {
-					if (function.isAST(Equal, 3)) {
-						function = F.Plus(((IAST) function).get(1), F.Times(F.CN1, ((IAST) function).get(2)));
-					}
-					try {
-						return F.List(F.Rule(list.get(1), Num.valueOf(findRoot(method, list, function))));
-					} catch (ConvergenceException e) {
-						if (Config.SHOW_STACKTRACE) {
-							e.printStackTrace();
-						}
-					} catch (MathException e) {
-						if (Config.SHOW_STACKTRACE) {
-							e.printStackTrace();
-						}
-					}
+			IExpr optionMethod = options.getOption("Method");
+			if (optionMethod != null && optionMethod instanceof ISymbol) {
+				method = ((ISymbol) optionMethod).toString();
+			} else {
+				if (ast.get(3) instanceof ISymbol) {
+					method = ast.get(3).toString();
 				}
+			}
+		}
+		if ((ast.get(2).isList())) {
+			IAST list = (IAST) ast.get(2);
+			IExpr function = ast.get(1);
+			if (list.size() == 4 && list.get(1) instanceof ISymbol && list.get(2) instanceof ISignedNumber
+					&& list.get(3) instanceof ISignedNumber) {
+				if (function.isAST(Equal, 3)) {
+					function = F.Plus(((IAST) function).get(1), F.Times(F.CN1, ((IAST) function).get(2)));
+				}
+				return F.List(F.Rule(list.get(1), Num.valueOf(findRoot(method, maxIterations, list, function))));
 			}
 		}
 		return null;
 	}
 
-	private double findRoot(String method, IAST list, IExpr function) throws ConvergenceException {
+	private double findRoot(String method, int maxIterations, IAST list, IExpr function) {
 		ISymbol xVar = (ISymbol) list.get(1);
 		ISignedNumber min = (ISignedNumber) list.get(2);
 		ISignedNumber max = (ISignedNumber) list.get(3);
@@ -101,9 +107,9 @@ public class FindRoot extends AbstractFunctionEvaluator implements IConstantHead
 		} else {
 			// default: NewtonSolver
 			BaseAbstractUnivariateRealSolver<DifferentiableUnivariateRealFunction> solver2 = new NewtonSolver();
-			return solver2.solve(Integer.MAX_VALUE, f, min.doubleValue(), max.doubleValue());
+			return solver2.solve(maxIterations, f, min.doubleValue(), max.doubleValue());
 		}
-		return solver.solve(Integer.MAX_VALUE, f, min.doubleValue(), max.doubleValue());
+		return solver.solve(maxIterations, f, min.doubleValue(), max.doubleValue());
 
 	}
 
