da5c3c976997b0d72efaaf4c45384dde2e360b73
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2011-10-02 22:49:32 +0000

    New JAS version 2.4.3788

--- trunk/matheclipse-core/src/main/java/org/matheclipse/core/convert/Expr2Object.java
+++ trunk/matheclipse-core/src/main/java/org/matheclipse/core/convert/Expr2Object.java
@@ -8,11 +8,26 @@ public class Expr2Object {
 	public static double[] toDoubleVector(IAST ast) throws WrongArgumentType {
 		double[] result = new double[ast.size() - 1];
 		for (int i = 1; i < ast.size(); i++) {
-			if (ast.get(i) instanceof ISignedNumber) {
+			if (ast.get(i).isSignedNumber()) {
 				result[i - 1] = ((ISignedNumber) ast.get(i)).doubleValue();
 			} else {
-				throw new WrongArgumentType(ast, ast.get(i), i, "Trying to convert the argument into the double number range: "
-						+ Double.MIN_VALUE + " - " + Double.MAX_VALUE);
+				throw new WrongArgumentType(ast, ast.get(i), i, "Conversion into a vector of double values not possible!");
+			}
+		}
+		return result;
+	}
+
+	public static double[][] toDoubleMatrix(IAST ast) throws WrongArgumentType {
+		int[] dim = ast.isMatrix();
+		double[][] result = new double[dim[0]][dim[1]];
+		for (int i = 1; i <= dim[0]; i++) {
+			IAST row = (IAST) ast.get(i);
+			for (int j = 1; i <= dim[1]; j++) {
+				if (row.get(j).isSignedNumber()) {
+					result[i - 1][j - 1] = ((ISignedNumber) row.get(j)).doubleValue();
+				} else {
+					throw new WrongArgumentType(ast, ast.get(i), i, "Conversion into a matrix of double values not possible!");
+				}
 			}
 		}
 		return result;
