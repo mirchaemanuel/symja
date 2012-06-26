d2fc38b7aeb0ae612e22419ee45c6b023922ca02
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2012-06-26 17:37:57 +0000

    New JAS version 2.4.3965

---
+++ jas/src/main/java/edu/jas/structure/FieldFactory.java
@@ -0,0 +1,17 @@
+/*
+ * $Id: FieldFactory.java 3803 2011-10-16 21:49:23Z kredel $
+ */
+
+package edu.jas.structure;
+
+
+/**
+ * Field factory interface.
+ * Defines test for field and access of characteristic.
+ * @author Heinz Kredel
+ */
+
+public interface FieldFactory<C extends RingElem<C>> 
+    extends RingFactory<C> {
+
+}
