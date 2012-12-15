ae8216128fe079332f4bec508826420bc6bf1c0e
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2012-12-15 20:48:22 +0000

    Use symja_android_library now

--- android/trunk/SymjaDroid/src/org/matheclipse/core/eval/exception/JASConversionException.java
+++
@@ -1,8 +0,0 @@
-package org.matheclipse.core.eval.exception;
-
-
-public class JASConversionException extends Throwable {
-	public JASConversionException() {
-		super("JAS conversion error");
-	}
-}
--- android/trunk/SymjaDroid/src/org/matheclipse/core/interfaces/IStringX.java
+++
@@ -1,9 +0,0 @@
-package org.matheclipse.core.interfaces;
-
-/**
- *  (I)nterface for a (String) e(X)pression
- * 
- */
-public interface IStringX extends IExpr {
-
-}
