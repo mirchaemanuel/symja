97875b18f8666e2b48863b89d1b8aa160a5b495f
axelclk <axelclk@a3086cb0-e1a3-11dd-9208-8f743628cd7a>
2013-05-20 12:00:52 +0000

    jas Version 2.5.4408

--- jas/src/main/java/edu/jas/application/SolvableResidue.java
+++ jas/src/main/java/edu/jas/application/SolvableResidue.java
@@ -1,34 +1,37 @@
 /*
- * $Id: Residue.java 4407 2013-04-30 10:34:27Z kredel $
+ * $Id: SolvableResidue.java 4407 2013-04-30 10:34:27Z kredel $
  */
 
 package edu.jas.application;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.jas.kern.PrettyPrint;
-import edu.jas.poly.GenPolynomial;
-import edu.jas.poly.PolyUtil;
+import edu.jas.poly.GenSolvablePolynomial;
 import edu.jas.structure.GcdRingElem;
+import edu.jas.structure.NotInvertibleException;
 
 
 /**
- * Residue ring element based on GenPolynomial with RingElem interface. Objects
- * of this class are (nearly) immutable.
+ * SolvableResidue ring element based on GenSolvablePolynomial with RingElem
+ * interface. Objects of this class are (nearly) immutable.
  * @author Heinz Kredel
  */
-public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>> {
+public class SolvableResidue<C extends GcdRingElem<C>> implements GcdRingElem<SolvableResidue<C>> {
 
 
     /**
-     * Residue class factory data structure.
+     * SolvableResidue class factory data structure.
      */
-    public final ResidueRing<C> ring;
+    public final SolvableResidueRing<C> ring;
 
 
     /**
      * Value part of the element data structure.
      */
-    public final GenPolynomial<C> val;
+    public final GenSolvablePolynomial<C> val;
 
 
     /**
@@ -39,36 +42,36 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
 
 
     /**
-     * The constructor creates a Residue object from a ring factory.
-     * @param r residue ring factory.
+     * The constructor creates a SolvableResidue object from a ring factory.
+     * @param r solvable residue ring factory.
      */
-    public Residue(ResidueRing<C> r) {
+    public SolvableResidue(SolvableResidueRing<C> r) {
         this(r, r.ring.getZERO(), 0);
     }
 
 
     /**
-     * The constructor creates a Residue object from a ring factory and a
-     * polynomial.
-     * @param r residue ring factory.
-     * @param a polynomial.
+     * The constructor creates a SolvableResidue object from a ring factory and
+     * a polynomial.
+     * @param r solvable residue ring factory.
+     * @param a solvable polynomial.
      */
-    public Residue(ResidueRing<C> r, GenPolynomial<C> a) {
+    public SolvableResidue(SolvableResidueRing<C> r, GenSolvablePolynomial<C> a) {
         this(r, a, -1);
     }
 
 
     /**
-     * The constructor creates a Residue object from a ring factory, a
+     * The constructor creates a SolvableResidue object from a ring factory, a
      * polynomial and an indicator if a is a unit.
-     * @param r residue ring factory.
-     * @param a polynomial.
+     * @param r solvable residue ring factory.
+     * @param a solvable polynomial.
      * @param u isunit indicator, -1, 0, 1.
      */
-    public Residue(ResidueRing<C> r, GenPolynomial<C> a, int u) {
+    public SolvableResidue(SolvableResidueRing<C> r, GenSolvablePolynomial<C> a, int u) {
         ring = r;
         val = ring.ideal.normalform(a); //.monic() no go
-        if ( u == 0 || u == 1 ) {
+        if (u == 0 || u == 1) {
             isunit = u;
             return;
         }
@@ -76,10 +79,10 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
             isunit = 0;
             return;
         }
-        if ( val.isUnit() ) {
-           isunit = 1;
-        //} else { // not possible
-           //isunit = 0;
+        if (val.isUnit()) {
+            isunit = 1;
+            //} else { // not possible
+            //isunit = 0;
         }
         isunit = -1;
     }
@@ -90,7 +93,7 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
      * @return factory for this Element.
      * @see edu.jas.structure.Element#factory()
      */
-    public ResidueRing<C> factory() {
+    public SolvableResidueRing<C> factory() {
         return ring;
     }
 
@@ -100,13 +103,13 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
      * @see java.lang.Object#clone()
      */
     @Override
-    public Residue<C> copy() {
-        return new Residue<C>(ring, val, isunit);
+    public SolvableResidue<C> copy() {
+        return new SolvableResidue<C>(ring, val, isunit);
     }
 
 
     /**
-     * Is Residue zero.
+     * Is SolvableResidue zero.
      * @return If this is 0 then true is returned, else false.
      * @see edu.jas.structure.RingElem#isZERO()
      */
@@ -116,7 +119,7 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
 
 
     /**
-     * Is Residue one.
+     * Is SolvableResidue one.
      * @return If this is 1 then true is returned, else false.
      * @see edu.jas.structure.RingElem#isONE()
      */
@@ -126,7 +129,7 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
 
 
     /**
-     * Is Residue unit.
+     * Is SolvableResidue unit.
      * @return If this is a unit then true is returned, else false.
      * @see edu.jas.structure.RingElem#isUnit()
      */
@@ -140,16 +143,16 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
         // not jet known
         boolean u = ring.ideal.isUnit(val);
         if (u) {
-            isunit = 1;
+            isunit = 1; // seems to be wrong for solvable polynomial rings
         } else {
             isunit = 0;
         }
-        return (u);
+        return isunit > 0;
     }
 
 
     /**
-     * Is Residue a constant.
+     * Is SolvableResidue a constant.
      * @return true if this.val is a constant polynomial, else false.
      */
     public boolean isConstant() {
@@ -166,7 +169,7 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
         if (PrettyPrint.isTrue()) {
             return val.toString(ring.ring.getVars());
         }
-        return "Residue[ " + val.toString() + " mod " + ring.toString() + " ]";
+        return "SolvableResidue[ " + val.toString() + " mod " + ring.toString() + " ]";
     }
 
 
@@ -175,11 +178,11 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
      * @return script compatible representation for this Element.
      * @see edu.jas.structure.Element#toScript()
      */
-    //JAVA6only: @Override
+    @Override
     public String toScript() {
         // Python case
         return val.toScript();
-        //         return "PolyResidue( " + val.toScript() 
+        //         return "PolySolvableResidue( " + val.toScript() 
         //                         + ", " + ring.toScript() + " )";
     }
 
@@ -189,7 +192,7 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
      * @return script compatible representation for this ElemFactory.
      * @see edu.jas.structure.Element#toScriptFactory()
      */
-    //JAVA6only: @Override
+    @Override
     public String toScriptFactory() {
         // Python case
         return factory().toScript();
@@ -197,14 +200,14 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
 
 
     /**
-     * Residue comparison.
-     * @param b Residue.
+     * SolvableResidue comparison.
+     * @param b SolvableResidue.
      * @return sign(this-b), 0 means that this and b are equivalent in this
      *         residue class ring.
      */
-    //JAVA6only: @Override
-    public int compareTo(Residue<C> b) {
-        GenPolynomial<C> v = b.val;
+    @Override
+    public int compareTo(SolvableResidue<C> b) {
+        GenSolvablePolynomial<C> v = b.val;
         if (!ring.equals(b.ring)) {
             v = ring.ideal.normalform(v);
         }
@@ -221,12 +224,12 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
     @Override
     @SuppressWarnings("unchecked")
     public boolean equals(Object b) {
-        if (!(b instanceof Residue)) {
+        if (!(b instanceof SolvableResidue)) {
             return false;
         }
-        Residue<C> a = null;
+        SolvableResidue<C> a = null;
         try {
-            a = (Residue<C>) b;
+            a = (SolvableResidue<C>) b;
         } catch (ClassCastException e) {
         }
         if (a == null) {
@@ -250,37 +253,37 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
 
 
     /**
-     * Residue absolute value.
+     * SolvableResidue absolute value.
      * @return the absolute value of this.
      * @see edu.jas.structure.RingElem#abs()
      */
-    public Residue<C> abs() {
-        return new Residue<C>(ring, val.abs(), isunit);
+    public SolvableResidue<C> abs() {
+        return new SolvableResidue<C>(ring, (GenSolvablePolynomial<C>) val.abs(), isunit);
     }
 
 
     /**
-     * Residue summation.
-     * @param S Residue.
+     * SolvableResidue summation.
+     * @param S SolvableResidue.
      * @return this+S.
      */
-    public Residue<C> sum(Residue<C> S) {
-        return new Residue<C>(ring, val.sum(S.val));
+    public SolvableResidue<C> sum(SolvableResidue<C> S) {
+        return new SolvableResidue<C>(ring, (GenSolvablePolynomial<C>) val.sum(S.val));
     }
 
 
     /**
-     * Residue negate.
+     * SolvableResidue negate.
      * @return -this.
      * @see edu.jas.structure.RingElem#negate()
      */
-    public Residue<C> negate() {
-        return new Residue<C>(ring, val.negate(), isunit);
+    public SolvableResidue<C> negate() {
+        return new SolvableResidue<C>(ring, (GenSolvablePolynomial<C>) val.negate(), isunit);
     }
 
 
     /**
-     * Residue signum.
+     * SolvableResidue signum.
      * @see edu.jas.structure.RingElem#signum()
      * @return signum(this).
      */
@@ -290,75 +293,85 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
 
 
     /**
-     * Residue subtraction.
-     * @param S Residue.
+     * SolvableResidue subtraction.
+     * @param S SolvableResidue.
      * @return this-S.
      */
-    public Residue<C> subtract(Residue<C> S) {
-        return new Residue<C>(ring, val.subtract(S.val));
+    public SolvableResidue<C> subtract(SolvableResidue<C> S) {
+        return new SolvableResidue<C>(ring, (GenSolvablePolynomial<C>) val.subtract(S.val));
     }
 
 
     /**
-     * Residue division.
-     * @param S Residue.
+     * SolvableResidue division.
+     * @param S SolvableResidue.
      * @return this/S.
      */
-    public Residue<C> divide(Residue<C> S) {
+    public SolvableResidue<C> divide(SolvableResidue<C> S) {
         if (ring.isField()) {
             return multiply(S.inverse());
         }
-        GenPolynomial<C> x = PolyUtil.<C> basePseudoDivide(val, S.val);
-        return new Residue<C>(ring, x);
+        List<GenSolvablePolynomial<C>> Q = new ArrayList<GenSolvablePolynomial<C>>(1);
+        Q.add(ring.ring.getZERO());
+        List<GenSolvablePolynomial<C>> V = new ArrayList<GenSolvablePolynomial<C>>(1);
+        V.add(val);
+        GenSolvablePolynomial<C> x = ring.bb.sred.leftNormalform(Q, V, S.val);
+        GenSolvablePolynomial<C> y = Q.get(0);
+        return new SolvableResidue<C>(ring, y);
     }
 
 
     /**
-     * Residue inverse.
+     * SolvableResidue inverse.
      * @see edu.jas.structure.RingElem#inverse()
      * @return S with S = 1/this if defined.
      */
-    public Residue<C> inverse() {
-        GenPolynomial<C> x = ring.ideal.inverse(val);
-        return new Residue<C>(ring, x, 1);
+    public SolvableResidue<C> inverse() {
+        GenSolvablePolynomial<C> x = ring.ideal.inverse(val);
+        SolvableResidue<C> xp = new SolvableResidue<C>(ring, x, 1);
+        if ( xp.isZERO() ) {
+            throw new NotInvertibleException("(" + x + ") * (" + val + ") = " + x.multiply(val) + " = 0 mod " + ring.ideal);
+        }
+        return xp;
     }
 
 
     /**
-     * Residue remainder.
-     * @param S Residue.
+     * SolvableResidue remainder.
+     * @param S SolvableResidue.
      * @return this - (this/S)*S.
      */
-    public Residue<C> remainder(Residue<C> S) {
-        //GenPolynomial<C> x = val.remainder( S.val );
-        GenPolynomial<C> x = PolyUtil.<C> baseSparsePseudoRemainder(val, S.val);
-        return new Residue<C>(ring, x);
+    public SolvableResidue<C> remainder(SolvableResidue<C> S) {
+        List<GenSolvablePolynomial<C>> V = new ArrayList<GenSolvablePolynomial<C>>(1);
+        V.add(val);
+        GenSolvablePolynomial<C> x = ring.bb.sred.leftNormalform(V, S.val);
+        return new SolvableResidue<C>(ring, x);
     }
 
 
     /**
-     * Residue multiplication.
-     * @param S Residue.
+     * SolvableResidue multiplication.
+     * @param S SolvableResidue.
      * @return this*S.
      */
-    public Residue<C> multiply(Residue<C> S) {
-        GenPolynomial<C> x = val.multiply(S.val);
+    public SolvableResidue<C> multiply(SolvableResidue<C> S) {
+        GenSolvablePolynomial<C> x = val.multiply(S.val);
         int i = -1;
         if (isunit == 1 && S.isunit == 1) {
             i = 1;
         } else if (isunit == 0 || S.isunit == 0) {
             i = 0;
         }
-        return new Residue<C>(ring, x, i);
+        return new SolvableResidue<C>(ring, x, i);
     }
 
 
     /**
-     * Residue monic.
+     * SolvableResidue monic.
      * @return this with monic value part.
      */
-    public Residue<C> monic() {
-        return new Residue<C>(ring, val.monic(), isunit);
+    public SolvableResidue<C> monic() {
+        return new SolvableResidue<C>(ring, (GenSolvablePolynomial<C>) val.monic(), isunit);
     }
 
 
@@ -367,18 +380,19 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
      * @param b other element.
      * @return gcd(this,b).
      */
-    public Residue<C> gcd(Residue<C> b) {
-        GenPolynomial<C> x = ring.engine.gcd(val, b.val);
-        int i = -1; // gcd might become a unit
-        if (x.isONE()) {
-            i = 1;
-        } else {
-            System.out.println("Residue gcd = " + x);
-        }
-        if (isunit == 1 && b.isunit == 1) {
-            i = 1;
-        }
-        return new Residue<C>(ring, x, i);
+    public SolvableResidue<C> gcd(SolvableResidue<C> b) {
+        throw new UnsupportedOperationException("gcd not implemented");
+        // GenSolvablePolynomial<C> x = ring.engine.gcd(val, b.val);
+        // int i = -1; // gcd might become a unit
+        // if (x.isONE()) {
+        //     i = 1;
+        // } else {
+        //     System.out.println("SolvableResidue gcd = " + x);
+        // }
+        // if (isunit == 1 && b.isunit == 1) {
+        //     i = 1;
+        // }
+        // return new SolvableResidue<C>(ring, x, i);
     }
 
 
@@ -388,7 +402,7 @@ public class Residue<C extends GcdRingElem<C>> implements GcdRingElem<Residue<C>
      * @param b other element.
      * @return [ gcd(this,b), c1, c2 ] with c1*this + c2*b = gcd(this,b).
      */
-    public Residue<C>[] egcd(Residue<C> b) {
+    public SolvableResidue<C>[] egcd(SolvableResidue<C> b) {
         throw new UnsupportedOperationException("egcd not implemented");
     }
 }
